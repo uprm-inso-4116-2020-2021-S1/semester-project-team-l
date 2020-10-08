86f0a5320f1c39b5b6dbb6cb5a7fc491ebf951ae
Julian E. Ramos Velez <julian.ramos1@upr.edu>
2020-10-08 14:56:52 -0400

    Added Spring Security and changed class names

--- Food4Help/demo/src/main/java/com/TeamL/demo/GreetingController.java
+++ Food4Help/demo/src/main/java/com/TeamL/demo/GreetingController.java
@@ -2,7 +2,6 @@ package com.TeamL.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.TeamL.demo.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
