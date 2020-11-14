06e03b00a2e4f3d83819a6dcbd4f84dd8be7ae1d
julian459 <julian.ramos1@upr.edu>
2020-11-14 19:09:41 -0400

    Fixed some errors from merge

--- Food4Help/demo/src/main/appFrontend/src/index.js
+++ Food4Help/demo/src/main/appFrontend/src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 import Company from './Screens/Company';
 import SignUp from './Screens/SignUp';
