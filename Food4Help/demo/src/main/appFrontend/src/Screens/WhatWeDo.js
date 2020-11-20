4274162c3747298755a7e25146dfe4f65c1d3d76
antho <anthony.rivera10@upr.edu>
2020-11-20 13:02:51 -0400

    Worked with What we doo.

--- Food4Help/demo/src/main/appFrontend/src/Screens/WhatWeDo.js
+++ Food4Help/demo/src/main/appFrontend/src/Screens/WhatWeDo.js
@@ -15,6 +15,9 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: 'column',
       alignItems: 'center',
     },
+  margin: {
+    marginBottom: theme.spacing(5)
+  },
 }));
 
 const sections = [
@@ -32,8 +35,17 @@ export default function WhatWeDo() {
       <Container maxWidth="lg">
         <Header title="What We Do" sections={sections} />
         <div className={classes.paper} >
+                 <Typography variant="h5"  component="div" className={classes.margin}>
+                    What is our goal with the page?
+                 </Typography>
                  <Typography variant="body1" component="div" style={{ height: '100vh' }} >
-                  This is What We Do official page.
+                 One of the factors that cause pollution and climate change is the food waste.
+                 While food waste can happen in individuals housegolds it not as big as done by corporations.
+                 Our goal with Food4Help is to create and give an interface where companies can communicate about
+                 their consumable food that they can give out to charity. That way the page can work as a medium.
+                 For charity organizations find and communicate with companies, supermarkets and fastfoods that are willing
+                 to help share their food instead of letting it to waste.
+
                 </Typography>
         </div>
       </Container>
