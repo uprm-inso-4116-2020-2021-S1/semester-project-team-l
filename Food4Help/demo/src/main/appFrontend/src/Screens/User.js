678e8b3fed10f59073d4b9ff862bb5c4d4c0e3ed
John Robles <john.robles1@upr.edu>
2020-11-22 18:38:30 -0400

    added more data to user profile

--- Food4Help/demo/src/main/appFrontend/src/Screens/User.js
+++ Food4Help/demo/src/main/appFrontend/src/Screens/User.js
@@ -10,6 +10,8 @@ import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField";
+import axios from "axios";
+import Select from "react-select";
 
 const useStyles = makeStyles((theme) => ({
   mainGrid: {
@@ -33,7 +35,12 @@ const useStyles = makeStyles((theme) => ({
       },
     buttonLayout: {
       textAlign: 'center'
-    }
+    },
+    select: {
+      width: "97%",
+      marginLeft: theme.spacing(1),
+      zIndex: 9999
+    },
 }));
 
 const sections = [
@@ -48,8 +55,68 @@ export default function User() {
   const [isEdit, setIsEdit] = useState(false);
   const [name, setName] = useState("Juan Perez Torres");
   const [email, setEmail] = useState("juan.perez@upr.edu");
+  const [password, setPassword] = useState("password");
+  const [entity, setEntity] = useState("Company");
+  const [nameEntity, setNameEntity] = useState("eat4less");
+  const [phone, setPhone] = useState("1234567890");
+  const [category, setCategory] = useState("fast-food");
   const [gender, setGender] = useState("Male");
   const [age, setAge] = useState("24");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [ID, setID] = useState(null);
+
+  const companyOptions = [
+    { value: "supermarket", label: "Supermarket" },
+    { value: "restaurant", label: "Restaurant" },
+    { value: "fastfood", label: "Fast-Food" },
+  ];
+  const entityOptions = [
+    { value: "company", label: "Company" },
+    { value: "organization", label: "Organization" },
+  ];
+
+  const fetchData = async () => {
+    let id = null;
+    await axios({
+      method: "GET",
+      url: "http://localhost:8080/getStatus",
+    }).then((response) => {
+      if (response.data) {
+        setLoggedIn(response.data);
+      }
+    });
+    await axios({
+      method: "GET",
+      url: "http://localhost:8080/getUser",
+    }).then((response) => {
+      if (response.data) {
+        setID(response.data);
+        id = response.data
+        console.log(id)
+      }
+    });
+    await axios({
+      method: "GET",
+      url: "http://localhost:8080/user/"+ id
+    }).then((response) => {
+      if (response.data) {
+        setCurrentUser(response.data);
+        console.log(response.data)
+      }
+    });
+    await axios({
+      method: "PUT",
+      url: "http://localhost:8080/updateStatus",
+      params: {
+        status: loggedIn,
+      }
+    }).then((response) => {
+      if (response.data) {
+        setLoggedIn(response.data);
+      }
+    });
+  };
 
   const handleEdit = () => {
     setIsEdit(true);
@@ -65,6 +132,26 @@ export default function User() {
 
   const emailChangeHandler = (e) => {
     setEmail(e.target.value);
+  };
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const entityChangeHandler = (e) => {
+    setEntity(e.target.value);
+  };
+
+  const nameEntityChangeHandler = (e) => {
+    setNameEntity(e.target.value);
+  };
+
+  const phoneChangeHandler = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const categoryChangeHandler = (e) => {
+    setCategory(e.target.value);
   };
 
   const genderChangeHandler = (e) => {
@@ -122,6 +209,83 @@ export default function User() {
                            name="email"
                            autoComplete="email"
                            autoFocus
+                         />
+                           }
+                           {!isEdit ? 
+                           <Typography variant="h6">
+                             Password: {password}
+                           </Typography> : 
+                           <TextField
+                           variant="outlined"
+                           margin="normal"
+                           required
+                           fullWidth
+                           value={password}
+                           type="text"
+                           onChange={passwordChangeHandler}
+                           id="password"
+                           label="Password"
+                           name="password"
+                           autoComplete="password"
+                           autoFocus
+                         />
+                           }
+                           {!isEdit ? 
+                           <Typography variant="h6">
+                             Entity: {entity}
+                           </Typography> : 
+                           <Select
+                           onChange={(e) => setEntity(e.value)}
+                           className={classes.select}
+                           options={entityOptions}
+                         />
+                           }
+                           {!isEdit ? 
+                           <Typography variant="h6">
+                             Company or Organization: {nameEntity}
+                           </Typography> : 
+                           <TextField
+                           variant="outlined"
+                           margin="normal"
+                           required
+                           fullWidth
+                           value={nameEntity}
+                           type="text"
+                           onChange={nameEntityChangeHandler}
+                           id="nameEntity"
+                           label="Name Entity"
+                           name="nameEntity"
+                           autoComplete="nameEntity"
+                           autoFocus
+                         />
+                           }
+                           {!isEdit ? 
+                           <Typography variant="h6">
+                             Phone Number: {phone}
+                           </Typography> : 
+                           <TextField
+                           variant="outlined"
+                           margin="normal"
+                           required
+                           fullWidth
+                           value={phone}
+                           type="text"
+                           onChange={phoneChangeHandler}
+                           id="phone"
+                           label="Phone"
+                           name="phone"
+                           autoComplete="phone"
+                           autoFocus
+                         />
+                           }
+                           {!isEdit ? 
+                           <Typography variant="h6">
+                             Category Company: {category}
+                           </Typography> : 
+                           <Select
+                           onChange={(e) => setCategory(e.value)}
+                           className={classes.select}
+                           options={companyOptions}
                          />
                            }
                           {!isEdit ? 
