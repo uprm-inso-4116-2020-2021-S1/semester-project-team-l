3f86a85a4533a5922995bfac46b6c7f25520a291
Valerie Reyes Ortiz <valerie.reyes5@upr.edu>
2020-11-24 17:36:36 -0400

    User's name appears in chat

--- Food4Help/demo/src/main/appFrontend/src/Components/ChatInputForm.js
+++ Food4Help/demo/src/main/appFrontend/src/Components/ChatInputForm.js
@@ -1,64 +1,77 @@
-import React from "react";
 import PropTypes from "prop-types";
-
+import Cookies from "js-cookie";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 export default class ChatInputForm extends React.Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    disabled: PropTypes.bool
-  };
 
-  state = {
-    username: "",
-    text: ""
-  };
+    state = {
+        username: "",
+        text: ""
+    };
+    static propTypes = {
+        onSubmit: PropTypes.func.isRequired,
+        disabled: PropTypes.bool,
+    };
 
-  handleInputChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
+    componentDidMount() {
+        axios
+            .get("http://localhost:3000/user/" + Cookies.get("User"))
+            .then((response) => {
+                console.log(response);
+                this.setState({ username: response.data.firstName });
+            });
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.onSubmit(this.state);
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value,
+        });
+    };
 
-    this.setState({
-      text: ""
-    });
-  };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            id="username"
-            type="text"
-            autoFocus={true}
-            required={true}
-            value={this.state.username}
-            onChange={this.handleInputChange}
-          />
-        </div>
+        this.setState({
+            text: "",
+        });
+    };
 
-        <div>
-          <label htmlFor="text">Message:</label>
-          <input
-            id="text"
-            type="text"
-            required={true}
-            value={this.state.text}
-            onChange={this.handleInputChange}
-          />
-        </div>
-
-        <div>
-          <button type="submit" disabled={this.props.disabled}>
-            Send
-          </button>
-        </div>
-      </form>
-    );
-  }
+    render() {
+        return ( <
+            form onSubmit = { this.handleSubmit } >
+            <
+            div >
+            <
+            label htmlFor = "name" > Name: < /label>{" "} <
+            input id = "name"
+            type = "text"
+            autoFocus = { true }
+            required = { true }
+            value = { this.state.username }
+            onChange = { this.handleInputChange }
+            readOnly /
+            >
+            <
+            /div>{" "} <
+            div >
+            <
+            label htmlFor = "text" > Message: < /label>{" "} <
+            input id = "text"
+            type = "text"
+            required = { true }
+            value = { this.state.text }
+            onChange = { this.handleInputChange }
+            />{" "} < /
+            div > { " " } <
+            div >
+            <
+            button type = "submit"
+            disabled = { this.props.disabled } >
+            Send { " " } <
+            /button>{" "} < /
+            div > { " " } <
+            /form>
+        );
+    }
 }
